faeaf5e9afc2a1d91f1d4c8505b679baaf19310b
Atm-0s <mikeymc@protonmail.com>
2025-08-13 13:55:34 +1000

    Added a split_nodes_delimiter function to conversions which takes a list of TextNodes and returns a list of TextNodes with split by their TextType. Only works for simple delimiters such as ** for bold and _ for italic, but is configured to run as specified in the function. Added some tests to test_conversions.

--- src/conversions.py
+++ src/conversions.py
@@ -18,3 +18,23 @@ def text_node_to_html_node(text_node: TextNode):
         return text_type_dict[text_node.text_type](text_node)
     else:
          raise ValueError(f"{text_node.text_type} is not a valid TextType")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            split = node.text.split(delimiter)
+            if len(split) % 2 == 0:
+                raise ValueError(f"Invalid markdown: unmatched delimiter '{delimiter}'")
+
+            for i, text in enumerate(split):
+                if text != "":
+                    if i % 2 == 0:
+                        new_node = TextNode(text, TextType.TEXT)
+                        new_nodes.append(new_node)
+                    else:
+                        new_node = TextNode(text, text_type)
+                        new_nodes.append(new_node)
+    return new_nodes
--- src/test_conversions.py
+++ src/test_conversions.py
@@ -1,5 +1,5 @@
 import unittest
-from conversions import text_node_to_html_node
+from conversions import text_node_to_html_node, split_nodes_delimiter
 from textnode import TextNode, TextType
 
 class TestConversionFunctions(unittest.TestCase):
@@ -29,3 +29,22 @@ class TestConversionFunctions(unittest.TestCase):
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "A link")
         self.assertEqual(html_node.props["href"], "https://google.com")
+
+    def test_split_nodes_code(self):
+        node = TextNode("This is a text with a `code` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+        TextNode("This is a text with a ", TextType.TEXT),
+        TextNode("code", TextType.CODE),
+        TextNode(" word", TextType.TEXT),
+        ])
+
+    def test_split_nodes_plain_text(self):
+        node = TextNode("This is plain text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [TextNode("This is plain text", TextType.TEXT)])
+
+    def test_split_nodes_error(self):
+        node = TextNode("This is not **correct", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
